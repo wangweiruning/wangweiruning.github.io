63706c5533491643c9a40eeecf33bce1ccdff0b7
wangweirunning <397149747@qq.com>
2019-06-19 10:49:22 +0800

    healthy_H5
    健康险移动端H5

---
+++ healthy/precache-manifest.4da30691810f256f56e4804e85433704.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cb051a8583a32f5cf43a",
+    "url": "./static/js/main.cb051a85.chunk.js"
+  },
+  {
+    "revision": "c02855e82589de428da0",
+    "url": "./static/js/1.c02855e8.chunk.js"
+  },
+  {
+    "revision": "cb051a8583a32f5cf43a",
+    "url": "./static/css/main.f5c9adf3.chunk.css"
+  },
+  {
+    "revision": "8e7999ff30fd0d72988fbb26b3df3652",
+    "url": "./index.html"
+  }
+];
